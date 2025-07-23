7eaf01308d7023f70ef10707aa7243bac31b38e5
Arman Hassanzadeh <armanhassanzadeh04@gmail.com>
2025-07-23 22:35:41 +0330

    Update database.py

--- src/database.py
+++ src/database.py
@@ -3,7 +3,7 @@ from uuid import uuid4
 import datetime as dt
 from . import scripts
 
-mongodb = MongoClient('127.0.0.1', 27017)
+mongodb = MongoClient('mongodb+srv://armanarmani2089:<db_password>@qhaem.csloyhi.mongodb.net/?retryWrites=true&w=majority&appName=qhaem')
 
 db = mongodb['qhaem']
 
